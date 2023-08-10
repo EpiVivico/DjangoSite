87eeccb86f58a9e810e25af841f396eed89f4c6f
Victor YU <bardo@MacBook-Pro-de-Victor.local>
2023-08-11 01:57:21 +0200

    Page is now cleaner and have images

--- sitelebardo/find_instances.py
+++ sitelebardo/find_instances.py
@@ -1,9 +1,13 @@
 from projects.models import Project
 
+def deleteAllProjectInstances():
+    Project.objects.all().delete()
+
 def findProjectInstances():
     total = 0
     qs = Project.objects.all()
     for instance in qs:
         print(instance)
 
+deleteAllProjectInstances()
 findProjectInstances()
--- sitelebardo/projects/generate_project_obj.py
+++ sitelebardo/projects/generate_project_obj.py
@@ -4,7 +4,7 @@ p1 = Project(
     title='My First Project',
     description='A web development project.',
     technology='Django',
-    image='img/project1.png'
+    image='img/key.jpg'
 )
 p1.save()
 
@@ -12,7 +12,7 @@ p2 = Project(
     title='My Second Project',
     description='Another web development project.',
     technology='Flask',
-    image='img/project2.png'
+    image='img/gaming.jpg'
 )
 p2.save()
 
@@ -21,6 +21,6 @@ p3 = Project(
     title='My Third Project',
     description='A final development project.',
     technology='Django',
-    image='img/project3.png'
+    image='img/purple.jpg'
 )
 p3.save()
